62516a85fa3b646edab64f4c2a518c4b9a37293a
lanakuge <maulananizhar27@gmail.com>
2023-06-26 14:52:14 +0700

    add env variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    ACCESS_TOKEN_SECRET:
+      "dydRcXeJfRwOWOam5CXwF4KZINa66DIA99emZbtbGsaL1ectjO6fIEeDT6hIEb2APdzJTSx6RVC3GsBUk86wcVmSxNrXoiyrQW9o",
+    REFRESH_TOKEN_SECRET:
+      "YPOHX1fJB15SIlvI4oDLKbveel8bBr2nxUjf669yk6ZHCnlTzqiQAD6Jn9qBRXRLIC3lMayl8VgmeIoTT8IiZgeuOmkLkruXMSxI",
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
