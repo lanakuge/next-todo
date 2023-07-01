f61f30c1392ac3df6eed035212814a04f7254e1c
lanakuge <maulananizhar27@gmail.com>
2023-07-01 10:55:26 +0700

    addd env host

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
       "dydRcXeJfRwOWOam5CXwF4KZINa66DIA99emZbtbGsaL1ectjO6fIEeDT6hIEb2APdzJTSx6RVC3GsBUk86wcVmSxNrXoiyrQW9o",
     REFRESH_TOKEN_SECRET:
       "YPOHX1fJB15SIlvI4oDLKbveel8bBr2nxUjf669yk6ZHCnlTzqiQAD6Jn9qBRXRLIC3lMayl8VgmeIoTT8IiZgeuOmkLkruXMSxI",
+    HOST: "http://localhost:3000/",
   },
   reactStrictMode: true,
 };
