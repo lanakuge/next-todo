c506e86a420c7ebbcaefd325fc78fdeda54245f1
lanakuge <maulananizhar27@gmail.com>
2023-07-01 10:55:53 +0700

    add loader styles

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/styles/hamburger.css";
 import "../../node_modules/aos/dist/aos.css";
+import "@/styles/loader.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
