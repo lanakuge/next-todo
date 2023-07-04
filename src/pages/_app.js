962e019773245e641f3cab616bc7293913885eef
lanakuge <maulananizhar27@gmail.com>
2023-07-04 14:28:52 +0700

    add checkbox component and style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import "@/styles/hamburger.css";
 import "../../node_modules/aos/dist/aos.css";
 import "@/styles/loader.css";
+import "@/styles/checkbox.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
